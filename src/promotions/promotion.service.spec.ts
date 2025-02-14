7be934e5e11c7004b9219f958501fd6e6b43511a
Martial59110 <floquet.martial@gmail.com>
2025-02-14 14:11:31 +0100

    test(86c23khuj): add promotion service test file

---
+++ src/promotions/promotion.service.spec.ts
@@ -0,0 +1,32 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { PromotionService } from './promotion.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Promotion } from './entities/promotion.entity';
+
+describe('PromotionService', () => {
+  let service: PromotionService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        PromotionService,
+        {
+          provide: getRepositoryToken(Promotion),
+          useValue: {
+            create: jest.fn(),
+            save: jest.fn(),
+            find: jest.fn(),
+            findOneBy: jest.fn(),
+            delete: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+
+    service = module.get<PromotionService>(PromotionService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+}); 
