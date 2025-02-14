235315369d36594da112c8e15d202da52f21942b
Martial59110 <floquet.martial@gmail.com>
2025-02-14 14:13:34 +0100

    feat(86c21k8bv): add promotion controller implementation

---
+++ src/promotions/promotion.controller.ts
@@ -0,0 +1,38 @@
+import { Controller, Get, Post, Body, Put, Param, Delete, NotFoundException } from '@nestjs/common';
+import { PromotionService } from './promotion.service';
+import { CreatePromotionDto } from './dto/create-promotion.dto';
+import { UpdatePromotionDto } from './dto/update-promotion.dto';
+
+@Controller('promotions')
+export class PromotionController {
+  constructor(private readonly promotionService: PromotionService) {}
+
+  @Post()
+  create(@Body() createPromotionDto: CreatePromotionDto) {
+    return this.promotionService.create(createPromotionDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.promotionService.findAll();
+  }
+
+  @Get(':uuid')
+  findOne(@Param('uuid') uuid: string) {
+    return this.promotionService.findOne(uuid);
+  }
+
+  @Put(':uuid')
+  async update(@Param('uuid') uuid: string, @Body() updatePromotionDto: UpdatePromotionDto) {
+    const promotion = await this.promotionService.update(uuid, updatePromotionDto);
+    if (!promotion) {
+      throw new NotFoundException(`Promotion with UUID "${uuid}" not found`);
+    }
+    return promotion;
+  }
+
+  @Delete(':uuid')
+  remove(@Param('uuid') uuid: string) {
+    return this.promotionService.remove(uuid);
+  }
+} 
