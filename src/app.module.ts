db705c41738bc116264a74481fc50265b197b0ed
MessaKami <messaoud.houri@outlook.fr>
2025-02-14 14:47:44 +0100

    feat(86c21g11r): integrate guilds-templates module into application

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
 import { GuildModule } from './guilds/guilds.module';
 import { CampusModule } from './campuses/campuses.module';
+import { GuildsTemplatesModule } from './guilds-templates/guilds-templates.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeOrmConfig),
     GuildModule,
     CampusModule,
+    GuildsTemplatesModule,
   ],
   controllers: [],
   providers: [],
