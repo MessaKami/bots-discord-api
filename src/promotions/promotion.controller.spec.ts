c222aba90a2d35d4c78336a4faecb728b7b00b00
Martial59110 <floquet.martial@gmail.com>
2025-02-14 14:11:12 +0100

    test(86c23khuj): add promotion controller test file

---
+++ src/promotions/promotion.controller.spec.ts
@@ -0,0 +1,31 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { PromotionController } from './promotion.controller';
+import { PromotionService } from './promotion.service';
+
+describe('PromotionController', () => {
+  let controller: PromotionController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [PromotionController],
+      providers: [
+        {
+          provide: PromotionService,
+          useValue: {
+            create: jest.fn(),
+            findAll: jest.fn(),
+            findOne: jest.fn(),
+            update: jest.fn(),
+            remove: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+
+    controller = module.get<PromotionController>(PromotionController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+}); 
