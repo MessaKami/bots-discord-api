87a376df9f9790322a9e1e680445db7218546e96
EnguerranSGG <enguerran.smagghe@hotmail.fr>
2025-02-15 16:15:31 +0100

    refactor(86c21k4fg): add MemberInformations module import in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
 import { GuildModule } from './guilds/guilds.module';
 import { CampusModule } from './campuses/campuses.module';
-import { MemberInformationsModule } from './member-informations/member-informations.module';
+import { MembersInformationsModule } from './members-informations/members-informations.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeOrmConfig),
     GuildModule,
     CampusModule,
-    MemberInformationsModule,
+    MembersInformationsModule,
   ],
   controllers: [],
   providers: [],
