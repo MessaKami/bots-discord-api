0ae68b437ffdeb92b3c8329d7fc4ea9b8b875693
Ayoub Laroussi <ayoublaroussipro@gmail.com>
2025-02-15 09:56:21 +0100

    feat(86c23xnw2): configure ModeratorActions module with TypeORM integration

--- src/moderator-actions/moderator-actions.module.ts
+++ src/moderator-actions/moderator-actions.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ModeratorActionsService } from './moderator-actions.service';
 import { ModeratorActionsController } from './moderator-actions.controller';
+import { ModeratorAction } from './entities/moderator-action.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ModeratorAction])],
   controllers: [ModeratorActionsController],
   providers: [ModeratorActionsService],
+  exports: [ModeratorActionsService],
 })
 export class ModeratorActionsModule {}
