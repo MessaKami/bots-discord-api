8e187f6e1aa082b7470f4e91b3d23083f1825580
Ayoub Laroussi <ayoublaroussipro@gmail.com>
2025-02-15 09:56:03 +0100

    feat(86c23xnta): create ModeratorAction entity with TypeORM configuration

--- src/moderator-actions/entities/moderator-action.entity.ts
+++ src/moderator-actions/entities/moderator-action.entity.ts
@@ -1,1 +1,32 @@
-export class ModeratorAction {}
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+import { ActionType } from '../dto/create-moderator-action.dto';
+
+@Entity('moderator_actions')
+export class ModeratorAction {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  userId: string;
+
+  @Column({
+    type: 'enum',
+    enum: ActionType,
+  })
+  actionType: ActionType;
+
+  @Column()
+  reason: string;
+
+  @Column({ nullable: true })
+  duration: string;
+
+  @Column({ nullable: true })
+  notes: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
