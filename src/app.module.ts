e864ee591768a79849599ce15880f58bda1bfd08
UnknOownU <149084644+UnknOownU@users.noreply.github.com.>
2025-02-17 19:25:09 +0100

    fix: update app.module with new feature modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ResourcesModule } from './resources/resources.module';
 import { ReportsModule } from './reports/reports.module';
 import { typeOrmConfig } from './config/typeorm.config';
+import { GuildModule } from './guilds/guilds.module';
+import { CampusModule } from './campuses/campuses.module';
+import { GuildsTemplatesModule } from './guilds-templates/guilds-templates.module';
+import { ModeratorActionsModule } from './moderator-actions/moderator-actions.module';
+import { MembersInformationsModule } from './members-informations/members-informations.module';
+import { CategoriesModule } from './categories/categories.module';
+import { AnswersModule } from './answers/answers.module';
+import { RolesModule } from './roles/roles.module';
+import { MembersModule } from './members/members.module';
+import { XpTransactionsModule } from './xp-transactions/xp-transactions.module';
+import { QuestionsModule } from './questions/questions.module';
 
 /**
  * Module principal de l'application
@@ -16,6 +27,17 @@ import { typeOrmConfig } from './config/typeorm.config';
     TypeOrmModule.forRoot(typeOrmConfig),
     ResourcesModule,
     ReportsModule,
+    GuildModule,
+    CampusModule,
+    GuildsTemplatesModule,
+    ModeratorActionsModule,
+    MembersInformationsModule,
+    CategoriesModule,
+    AnswersModule,
+    RolesModule,
+    MembersModule,
+    XpTransactionsModule,
+    QuestionsModule,
   ],
   controllers: [],
   providers: [],
