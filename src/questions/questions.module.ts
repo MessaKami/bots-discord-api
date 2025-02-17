a50fd5f6f295e606443600e7d7fc0e6b266172da
Boris Betremieux <146920742+BborisB@users.noreply.github.com>
2025-02-17 20:58:29 +0100

    fix(86c21mx51): fix entity name in the import section
    T86c21mx51

--- src/questions/questions.module.ts
+++ src/questions/questions.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { QuestionsService } from './questions.service';
 import { QuestionsController } from './questions.controller';
-import { Questions } from './entities/question.entity';
+import { Question } from './entities/question.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Questions])],
