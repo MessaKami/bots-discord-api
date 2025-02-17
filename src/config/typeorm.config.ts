a14a22b51019f701d66f6cff577c70121016aa5a
Yohan Ferdin <yohan.ferdin@hotmail.fr>
2025-02-17 16:22:07 +0100

    fix(86c23ju11): update PostgreSQL port from 5433 to 5432 in TypeORM configuration

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -6,7 +6,7 @@ dotenv.config({ path: '.env.development' });
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
   host: process.env.DB_HOST,
-  port: 5433,
+  port: 5432,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
