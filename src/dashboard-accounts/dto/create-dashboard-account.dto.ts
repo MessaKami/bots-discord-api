7eb894986894894a7c387bd8d98968ba2f45ee38
Aurore Reynier <reynier.aurore@gmail.com>
2025-02-18 14:06:19 +0100

    feat(86c2298x5): enable swagger in dashboard-account dto

--- src/dashboard-accounts/dto/create-dashboard-account.dto.ts
+++ src/dashboard-accounts/dto/create-dashboard-account.dto.ts
@@ -1,32 +1,31 @@
 import { IsString, IsEmail, MinLength, IsUUID, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateDashboardAccountDto {
-    // Swagger
-    // @ApiProperty({
-    //     description: 'Identifiant unique du compte du dashboard',
-    //     type: String,
-    //     example: '123e4567-e89b-12d3-a456-426614174000',
-    // })
+    @ApiProperty({
+        description: 'Identifiant unique du compte du dashboard',
+        type: String,
+        example: '123e4567-e89b-12d3-a456-426614174000',
+    })
     @IsNotEmpty()
     @IsUUID()
     uuidDashboardAccount: string;
 
-    // Swagger
-    // @ApiProperty({
-    //     description: 'Email associé au compte du dashboard',
-    //     type: String,
-    //     example: 'test@example.com',
-    // })
+
+    @ApiProperty({
+        description: 'Email associé au compte du dashboard',
+        type: String,
+        example: 'test@example.com',
+    })
     @IsNotEmpty()
     @IsEmail()
     email: string;
 
-    // Swagger
-    // @ApiProperty({
-    //     description: 'Mot de passe associé au compte du dashboard',
-    //     type: String,
-    //     example: 'password123',
-    // })
+    @ApiProperty({
+        description: 'Mot de passe associé au compte du dashboard',
+        type: String,
+        example: 'password123',
+    })
     @IsNotEmpty()
     @IsString()
     @MinLength(8)
