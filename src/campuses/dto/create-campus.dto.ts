499a2b7fcf598cbbdae49d1816c8b7be76c745dc
MessaKami <messaoud.houri@outlook.fr>
2025-02-19 10:24:22 +0100

    chore: cleaning ficihers and updating the campus dto

--- src/campuses/dto/create-campus.dto.ts
+++ src/campuses/dto/create-campus.dto.ts
@@ -1,9 +1,25 @@
 import { IsString, MaxLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateCampusDto {
 
+  @ApiProperty({
+    description: 'Nom du campus',
+    example: 'Simplon Paris',
+    maxLength: 50
+  })
   @IsString()
   @MaxLength(50)
   name: string;
+
+  @ApiProperty({
+    description: 'Adresse du campus',
+    example: '55 Rue de Vincennes, 93100 Montreuil',
+    maxLength: 200,
+    required: false
+  })
+  @IsString()
+  @MaxLength(200)
+  address?: string;
 
 }
