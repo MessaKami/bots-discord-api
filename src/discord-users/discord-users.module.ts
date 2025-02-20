c0c215a7e0993b850f605d36cfb62747ee18aeb1
Martial59110 <floquet.martial@gmail.com>
2025-02-20 09:56:59 +0100

    feat(86c21k5hd): add discord-users module

---
+++ src/discord-users/discord-users.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DiscordUsersService } from './discord-users.service';
+import { DiscordUsersController } from './discord-users.controller';
+import { DiscordUser } from './entities/discord-user.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([DiscordUser])],
+  controllers: [DiscordUsersController],
+  providers: [DiscordUsersService],
+})
+export class DiscordUsersModule {} 
