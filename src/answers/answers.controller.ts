a61e754eacf826d8f76547a5e0d9c0ecc9bc05ba
Srekaens <justindidelot8@outlook.fr>
2025-02-20 18:03:36 +0100

    chore(86c21mpmv): add HttpStatus import for swagger documentation
    T86c21mpmv
    
    - Add HttpStatus import from @nestjs/common
    
    - Prepare for swagger status codes implementation

--- src/answers/answers.controller.ts
+++ src/answers/answers.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Put, Param, Delete, NotFoundException } from '@nestjs/common';
+import { Controller, Get, Post, Body, Put, Param, Delete, NotFoundException, HttpStatus } from '@nestjs/common';
 import { AnswersService } from './answers.service';
 import { CreateAnswerDto } from './dto/create-answer.dto';
 import { UpdateAnswerDto } from './dto/update-answer.dto';
