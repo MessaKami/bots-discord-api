36700faca3b02fdaf178a09815aedc5a6b4d1870
UnknOownU <149084644+UnknOownU@users.noreply.github.com>
2025-02-21 15:49:25 +0100

    feat(86c27g3mz): remove unused imports in resource module

--- src/resources/resources.module.ts
+++ src/resources/resources.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ResourcesService } from './resources.service';
 import { ResourcesController } from './resources.controller';
 import { Resource } from './entities/resource.entity';
+import { Member } from '../members/entities/member.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Resource])],
+  imports: [TypeOrmModule.forFeature([Resource, Member])],
   controllers: [ResourcesController],
   providers: [ResourcesService],
   exports: [ResourcesService],
