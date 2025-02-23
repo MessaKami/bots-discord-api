3338a92751697a0de048cc095553b00f58746ea1
EnguerranSGG <enguerran.smagghe@hotmail.fr>
2025-02-23 21:01:40 +0100

    feat(86c28dcae): import RolesModule in CoursesModule

--- src/courses/courses.module.ts
+++ src/courses/courses.module.ts
@@ -5,10 +5,12 @@ import { CoursesService } from "./courses.service";
 import { Course } from "./entities/course.entity";
 import { Category } from "../categories/entities/category.entity";
 import { Guild } from "../guilds/entities/guild.entity";
+import { RolesModule } from "../roles/roles.module";
 
 @Module({
     imports: [
-        TypeOrmModule.forFeature([Course, Category, Guild]), 
+        TypeOrmModule.forFeature([Course, Category, Guild]),
+        RolesModule
     ],
     controllers: [CoursesController],
     providers: [CoursesService],
