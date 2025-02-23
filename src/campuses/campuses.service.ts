b7d51639b69d0c44d5b69eaa8ee89b4c4e5130a3
EnguerranSGG <enguerran.smagghe@hotmail.fr>
2025-02-23 20:27:36 +0100

    feat(86c28dcae): enhance campus creation with automatic role generation

--- src/campuses/campuses.service.ts
+++ src/campuses/campuses.service.ts
@@ -1,32 +1,56 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateCampusDto } from './dto/create-campus.dto';
 import { UpdateCampusDto } from './dto/update-campus.dto';
 import { Campus } from './entities/campus.entity';
+import { Role } from 'src/roles/entities/role.entity';
 
 @Injectable()
 export class CampusesService {
   constructor(
     @InjectRepository(Campus)
     private campusRepository: Repository<Campus>,
+
+    @InjectRepository(Role)
+    private roleRepository: Repository<Role>,
   ) {}
 
-  create(createCampusDto: CreateCampusDto) {
-    const campus = this.campusRepository.create(createCampusDto);
-    return this.campusRepository.save(campus);
+  async create(createCampusDto: CreateCampusDto): Promise<Campus> {
+    try {
+      const newRole = this.roleRepository.create({
+        uuid_role: createCampusDto.uuid_role, 
+        uuid_guild: createCampusDto.uuid_guild,
+        name: createCampusDto.name,
+        member_count: "0",
+        role_position: "0",
+        hoist: false,
+        color: "#000000",
+      });
+
+      const savedRole = await this.roleRepository.save(newRole);
+
+      const newCampus = this.campusRepository.create({
+        ...createCampusDto,
+        uuid_role: savedRole.uuid_role,
+      });
+
+      return await this.campusRepository.save(newCampus);
+    } catch (error) {
+      throw new BadRequestException('Erreur lors de la création du campus: ' + error.message);
+    }
   }
 
   findAll() {
     return this.campusRepository.find();
   }
 
-  findOne(uuid: string) {
-    return this.campusRepository.findOneBy({ uuid });
+  findOne(uuid_campus: string) {
+    return this.campusRepository.findOneBy({ uuid_campus });
   }
 
   async update(uuid: string, updateCampusDto: UpdateCampusDto) {
-    const campus = await this.campusRepository.findOneBy({ uuid });
+    const campus = await this.campusRepository.findOneBy({ uuid_campus : uuid });
     if (!campus) {
       return null;
     }
@@ -34,7 +58,7 @@ export class CampusesService {
     return this.campusRepository.save(campus);
   }
 
-  remove(uuid: string) {
-    return this.campusRepository.delete({ uuid });
+  remove(uuid_campus: string) {
+    return this.campusRepository.delete({ uuid_campus });
   }
 }
