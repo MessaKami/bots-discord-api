5c84c498c62900631cdad4f1c5ac0bfac15da82c
EnguerranSGG <enguerran.smagghe@hotmail.fr>
2025-02-23 20:27:20 +0100

    feat(86c28dcae): import RolesModule in CampusesModule

--- src/campuses/campuses.module.ts
+++ src/campuses/campuses.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CampusesService } from './campuses.service';
 import { CampusesController } from './campuses.controller';
 import { Campus } from './entities/campus.entity';
+import { RolesModule } from 'src/roles/roles.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Campus])],
+  imports: [TypeOrmModule.forFeature([Campus]), RolesModule],
   controllers: [CampusesController],
   providers: [CampusesService],
 })
