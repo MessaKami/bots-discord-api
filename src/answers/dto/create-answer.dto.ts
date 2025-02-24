81ca691559637877458060893607337834e4bc6a
Srekaens <justindidelot8@outlook.fr>
2025-02-24 10:50:24 +0100

    refactor(86c21mpmv): remove ApiProperty decorator for questionUuid
    T#86c21mpmv
    
    - Remove ApiProperty decorator for questionUuid as it's inherited from PickableInternUUIDFields

--- src/answers/dto/create-answer.dto.ts
+++ src/answers/dto/create-answer.dto.ts
@@ -14,12 +14,4 @@ export class CreateAnswerDto extends PickType(PickableInternUUIDFields, [
   @IsString({message: 'Le contenu de la réponse doit être une chaîne de caractères'})
   @MaxLength(50, {message: 'Le contenu de la réponse ne doit pas dépasser 50 caractères'})
   content: string;
-
-  @ApiProperty({
-    description: 'UUID de la question associée à cette réponse',
-    example: '123e4567-e89b-12d3-a456-426614174000',
-    minLength: 36,
-    maxLength: 36,
-    type: String
-  })
 }
