3b5d04b242e9a6cefef01d9d5e07f531894f4a66
MessaKami <messaoud.houri@outlook.fr>
2025-02-24 13:11:24 +0100

    test(86c21x3p8): update xp transactions tests

--- src/xp-transactions/xp-transactions.controller.ts
+++ src/xp-transactions/xp-transactions.controller.ts
@@ -23,7 +23,7 @@ export class XpTransactionsController {
           transaction_type: 'GAIN',
           transaction_value: 100,
           createdAt: '2024-03-14T12:00:00Z',
-          uuid_member: '123e4567-e89b-12d3-a456-426614174000',
+          uuidMember: '123e4567-e89b-12d3-a456-426614174000',
           member: {
             uuid: '123e4567-e89b-12d3-a456-426614174000',
             guild_username: 'JohnDoe',
